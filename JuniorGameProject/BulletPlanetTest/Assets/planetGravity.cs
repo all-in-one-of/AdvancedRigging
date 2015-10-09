e72ebf753c6c0a586772b0dbd8b8cc1aa9c7d81f
Tom Callister <cardboardman01@gmail.com>
2015-10-08 23:54:18 -0600

    Initial prototyping for Junior Game Project, specifically trying out various bullet trajectory characteristics for best gameplay

---
+++ JuniorGameProject/BulletPlanetTest/Assets/planetGravity.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class planetGravity : MonoBehaviour {
+
+	public float pullRadius = 100.0f;
+	public float pullForce = 3.0f;
+	public Rigidbody gravity;
+
+	// Use this for initialization
+	void Start () {
+		gravity = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+	void FixedUpdate () {
+
+		foreach (Collider collider in Physics.OverlapSphere (transform.position, pullRadius))
+		{
+			Vector3 forceDirection = transform.position - collider.transform.position;
+			gravity.AddForce(forceDirection.normalized * pullForce * Time.fixedDeltaTime);
+		}
+	}
+}
